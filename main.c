305c047f28807a0b3ae3a448ae25c73abd5d5fb5
MikLz69 <mtkongsted@gmail.com>
2022-08-01 10:49:33 +0200

    finished history

--- main.c
+++ main.c
@@ -77,6 +77,7 @@ int main()
     Date history[64] = {{.day = 0, .month = 0, .year = 0}};
     int daysInMonths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     Date EPOCH = {.day = 1, .month = 1, .year = 1970};
+    bool hasCheckedIn = false;
     
     char buffer[128] = "";
     while (1) {
@@ -84,12 +85,19 @@ int main()
         fgets(buffer, 128, stdin);
         sanitize_input(buffer);
         if (!strncmp("checkin", buffer, 7)) {
+            if (!hasCheckedIn) {
             checkinTime = time(NULL);
             history[dateCounter] = date_encoder(checkinTime, EPOCH, daysInMonths);
             date_print("You checked in at", history[dateCounter]);
             dateCounter++;
+            hasCheckedIn = true;
+            }
+            else {
+                printf("You already have checked in\n");
+            }
         }
         if (!strncmp("checkout", buffer, 8)) {
+            if (hasCheckedIn) {
             timespan_decoder(&timeSpan);
             timeSpan.seconds += calc_checkout_time(time(NULL), checkinTime);
             timespan_encoder(&timeSpan);
@@ -97,6 +105,12 @@ int main()
             date_print("You checked out at", history[dateCounter]);
             dateCounter++;
             timespan_print(timeSpan);
+            hasCheckedIn = false;
+
+            }
+            else {
+                printf("You have not checked in\n");
+            }
         }
         if (!strncmp("history", buffer, 7)) {
             int historyLength = sizeof history / sizeof *history;
@@ -106,11 +120,11 @@ int main()
                 }
                 if (i % 2)
                 date_print("checkout", history[i]);
-                else {
+                else 
                 date_print("checkin", history[i]);
-                }
                 
             }
+            timespan_print(timeSpan);
         }
     }
 }
